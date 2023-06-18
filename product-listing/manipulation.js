e25bf47d67b59dbbdcd33f0219db9d8aebab911e
Rajnish Kumar <rajnishmsrit@gmail.com>
2023-06-18 11:08:47 +0530

    working

--- product-listing/manipulation.js
+++ product-listing/manipulation.js
@@ -3,19 +3,14 @@ const updateProductListing = async () => {
   console.log(await getProducts());
   const {products} = await getProducts();
   const resultsId = document.getElementById("searchResults");
+  let containerItem = "";
   products.map((item) => {
-    const productDiv = document.createElement("div");
-    productDiv.classList.add("container-item");
-    const productImageElement = document.createElement("img");
-    productImageElement.src = item.thumbnail;
-    const productLabelElement = document.createElement("label");
-    const productLabelTextElement = document.createTextNode(item.title);
-    const productBreakElement = document.createElement("br")
-    productLabelElement.append(productLabelTextElement);
-    productDiv.append(productImageElement);
-    productDiv.append(productBreakElement);
-    productDiv.append(productLabelElement);
-    resultsId.append(productDiv);
+    containerItem += `<div class="container-item">
+    <img src="${item.thumbnail}"><br>
+    <label>${item.title}</label></div>
+    `;
   })
+  resultsId.setHTML(containerItem);
 }
+
 updateProductListing();
